1720f14e4371c4e0e74bcf6ce10b521f0923c9b5
Kevin Konrad <Kevintkonrad@gmail.com>
2017-01-14 00:46:50 -0500

    Turn attack order into a universal order class

---
+++ standardBot/Order.java
@@ -0,0 +1,25 @@
+package standardBot;
+
+import battlecode.common.MapLocation;
+
+public class Order {
+	public int type;
+	public MapLocation location;
+	public int TTL;
+	public int count;
+	public Order(int memoryData) {
+		type = Utilities.bitInterval(memoryData, 29, 32);
+		TTL = Utilities.bitInterval(memoryData, 24, 28);
+		location = Utilities.intToTarget(Utilities.bitInterval(memoryData, 6, 23));
+		count = Utilities.bitInterval(memoryData, 0, 5);
+	}
+	public Order(int t, MapLocation loc, int time, int number){
+		type = t;
+		location = loc;
+		TTL = time;
+		count = number;
+	}
+	public int toInt(){
+		return type * (int) Math.pow(2, 29) + TTL * (int) Math.pow(2, 24) + (int) Utilities.targetToInt(location) * (int) Math.pow(2, 6) + count;
+	}
+}
