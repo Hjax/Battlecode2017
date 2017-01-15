0ea90c99c22f23944881d3e7e8a42f5f2bd20dea
Kevin Konrad <Kevintkonrad@gmail.com>
2017-01-14 21:13:16 -0500

    scouts cant pentad, remove code

--- standardBot/Scout.java
+++ standardBot/Scout.java
@@ -39,19 +39,11 @@ public class Scout extends Bot{
                 {
                     MapLocation target = robots[0].location;
                     // And we have enough bullets, and haven't attacked yet this turn...;
-                    if (rc.getLocation().isWithinDistance(target, 5.5f)){
-                    	if (rc.canFirePentadShot() && !Utilities.willHitAlly(rc.getLocation(), rc.getLocation().directionTo(target), rc.getLocation().distanceTo(target))) {
-                    		rc.firePentadShot(rc.getLocation().directionTo(target));
-                    	}
-                    }
                     if (rc.canFireSingleShot() && !Utilities.willHitAlly(rc.getLocation(), rc.getLocation().directionTo(target), rc.getLocation().distanceTo(target))) {
                         // ...Then fire a bullet in the direction of the enemy.
                         rc.fireSingleShot(rc.getLocation().directionTo(target));
                     }
                 }
-
-
-
 
             } catch (Exception e) {
                 System.out.println("Scout Exception");
